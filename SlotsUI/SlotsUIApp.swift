53d33d8aa934cbd129af8b7bd2101154f12ff9f7
SwiftDom <domanik.johnson@icloud.com>
2021-05-28 16:43:35 -0400

    Added project

---
+++ SlotsUI/SlotsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsUIApp.swift
+//  SlotsUI
+//
+//  Created by Domanik Johnson on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
